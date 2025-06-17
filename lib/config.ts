e880af21e0c7d884c3e863ba877b7cde87f53ab8
JOHNzPGG <79080120+JOHNzPGG@users.noreply.github.com>
2025-06-17 19:28:14 +0200

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ export const config = {
    port: process.env.PORT || 3100,
    supportedDevicesNum: 17,
    JwtSecret: "secret",
-   databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://twwai:KTp5wYwutrLHPLT@cluster0.ooees.mongodb.net/IoT?retryWrites=true&w=majority',
+   databaseUrl: process.env.MONGODB_URI || 'mongodb secret',
    // JwtSecret: process.env.JWT_SECRET || 'defaultSecretKey',
    //  databaseUrl: process.env.MONGODB_URI || 'ADRES MONGODB',
     tokenExpiration: '3h' as const,       // ważność tokena JWT
